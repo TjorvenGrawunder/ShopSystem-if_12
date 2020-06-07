25a46908b4248a0fb996efb45ec3363a5ffc27f8
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2020-06-07 16:45:49 +0200

    Added Gui

--- Shop System FX/src/application/Main.java
+++ Shop System FX/src/application/Main.java
@@ -1,21 +1,41 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		mainWindow();
+	}
+	
+	public void mainWindow() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("LoginWindow.fxml"));
+			AnchorPane pane = loader.load();
+			primaryStage.setMinHeight(400.00);
+			primaryStage.setMinWidth(300.00);
+			
+			MainWindowController mainWindowController = new MainWindowController();
+			loader.setController(mainWindowController);
+			mainWindowController.setMain(this);
+			
+			Scene scene = new Scene(pane);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
