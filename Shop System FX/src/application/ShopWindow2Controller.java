06cdca62b84d5cf576e6e5b3f3829a30037733ae
Marco <marconiehaus@outlook.de>
2020-06-25 14:01:30 +0200

    Anpassungen im ShopWindow
    2. ShopWindow für User mit Admin-Rechten erstellt
    
    Test-Datei für das Shop-Window erstellt, damit man am Design arbeiten
    kann
    
    Klasse Produkte mit Konstruktor erstellt
    
    ShopWindow2Controller erstellt(erbt von ShopWindowController)

---
+++ Shop System FX/src/application/ShopWindow2Controller.java
@@ -0,0 +1,9 @@
+package application;
+/*
+ * Controller für das ShopWindow der User mit Admin-Rechten.
+ * Soll neben den Funktionen für den normalen User auch noch
+ * weitere Funktionen aufweisen, die nur User mit Admin-Rechten nutzen können
+ */
+public class ShopWindow2Controller extends ShopWindowController{
+	
+}
