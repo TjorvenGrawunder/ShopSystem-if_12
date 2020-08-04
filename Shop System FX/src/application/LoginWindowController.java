e2f1dd624859232d8eaec83e7e490eead014ecc5
Marco <marconiehaus@outlook.de>
2020-08-04 19:43:31 +0200

    Logout Button hinzugefügt

--- Shop System FX/src/application/LoginWindowController.java
+++ Shop System FX/src/application/LoginWindowController.java
@@ -1,12 +1,15 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Effect;
@@ -14,7 +17,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
-public class LoginWindowController {
+public class LoginWindowController implements Initializable {
 
 	SQLiteJDBCDatabase sqlDatabase = SQLiteJDBCDatabase.getInstance();
 
@@ -77,5 +80,10 @@ public class LoginWindowController {
 		return correctPassword;
 
 	}
-
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- Shop System FX/src/application/ShopWindowController.java
+++ Shop System FX/src/application/ShopWindowController.java
@@ -1,19 +1,28 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class ShopWindowController implements Initializable {
 
+	@FXML
+	private AnchorPane shopPane;
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 	}
-
+	
+	public void logoutButtonClick(ActionEvent event) throws IOException {
+		AnchorPane loginPane = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+		shopPane.getChildren().setAll(loginPane);
+	}
 }
