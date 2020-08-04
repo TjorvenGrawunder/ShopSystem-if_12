3e596d56d098b07b5d62854936631aca48a093f1
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2020-08-04 20:21:58 +0200

    random push

--- Shop System FX/src/application/ShopWindow2Controller.java
+++ Shop System FX/src/application/ShopWindow2Controller.java
@@ -1,9 +1,10 @@
 package application;
+
 /*
  * Controller für das ShopWindow der User mit Admin-Rechten.
  * Soll neben den Funktionen für den normalen User auch noch
  * weitere Funktionen aufweisen, die nur User mit Admin-Rechten nutzen können
  */
-public class ShopWindow2Controller extends ShopWindowController{
+public class ShopWindow2Controller extends ShopWindowController {
 	
 }
