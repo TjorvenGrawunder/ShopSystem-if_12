006788cbbb0db5892328f65b92dcc53d98881a20
Marco <marconiehaus@outlook.de>
2020-08-05 15:17:47 +0200

    Tjorvens Fehler behoben ;)

--- Shop System FX/src/application/ShopWindow2Controller.java
+++ Shop System FX/src/application/ShopWindow2Controller.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,12 +29,21 @@ public class ShopWindow2Controller extends ShopWindowController {
 	private TextField priceField;
 	@FXML
 	private TextField categoryField;
+	@FXML
+	private TextField productIdShoppingCart2;
+	@FXML
+	private ComboBox<String> sizeCombobox2;
+	public ArrayList<WarenkorbElement> shoppingCartList2 = new ArrayList();
 	
 	public void addProductButtonClick(ActionEvent event) throws IOException {
 		String productname = productNameField.getText();
 		int price = Integer.parseInt(priceField.getText());
 		String category = categoryField.getText();
 		sqlDatabase.addProducts( productname, price, category);
+	}
+	public void addToShoppingCartButtonClick2(ActionEvent event) throws IOException {
+		WarenkorbElement element = new WarenkorbElement(Integer.parseInt(productIdShoppingCart2.getText()), sizeCombobox2.getValue());
+		shoppingCartList2.add(element);
 	}
 }
 
