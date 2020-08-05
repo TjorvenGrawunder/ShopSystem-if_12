5b56f740b2d1b67e125d90057ea78a71fa39a966
Marco <marconiehaus@outlook.de>
2020-08-05 13:12:14 +0200

    Idee für fürs Produkt hinzufügen

--- Shop System FX/src/application/ShopWindow2Controller.java
+++ Shop System FX/src/application/ShopWindow2Controller.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /*
  * Controller für das ShopWindow der User mit Admin-Rechten.
  * Soll neben den Funktionen für den normalen User auch noch
@@ -7,4 +12,15 @@ package application;
  */
 public class ShopWindow2Controller extends ShopWindowController {
 	
+	SQLiteJDBCDatabase sqlDatabase = SQLiteJDBCDatabase.getInstance();
+	
+	@FXML
+	private String productname;
+	private int price;
+	private String category;
+	
+	public void addProductButtonClick(ActionEvent event) throws IOException {
+		sqlDatabase.createProductsTable();
+		sqlDatabase.addProducts( productname, price, category);
+	}
 }
