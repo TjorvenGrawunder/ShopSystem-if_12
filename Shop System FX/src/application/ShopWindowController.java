993a26d4c998f4daa82c969679b6fbb0295ff4b6
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2020-08-05 15:12:37 +0200

    "Zum Warenkorb hinzufügen" Button hinzugefügt und mit Funktionen
    versehen.
    BUG: Admin hat noch nicht die Möglichkeit einen Warenkorb zu erstellen

--- Shop System FX/src/application/ShopWindowController.java
+++ Shop System FX/src/application/ShopWindowController.java
@@ -2,19 +2,27 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class ShopWindowController implements Initializable {
 
 	@FXML
 	private AnchorPane shopPane;
+	@FXML
+	private TextField productIdShoppingCart;
+	@FXML
+	private ComboBox<String> sizeCombobox;
+	public ArrayList<WarenkorbElement> shoppingCartList = new ArrayList();
 	
 	
 	@Override
@@ -24,5 +32,10 @@ public class ShopWindowController implements Initializable {
 	public void logoutButtonClick(ActionEvent event) throws IOException {
 		AnchorPane loginPane = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
 		shopPane.getChildren().setAll(loginPane);
+	}
+	
+	public void addToShoppingCartButtonClick(ActionEvent event) throws IOException {
+		WarenkorbElement element = new WarenkorbElement(Integer.parseInt(productIdShoppingCart.getText()), sizeCombobox.getValue());
+		shoppingCartList.add(element);
 	}
 }
